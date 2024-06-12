e73517fbee95cb34e449f6fe293c2ad934b2eb69
James LaFarr <jlafarr99@gmail.com>
2024-06-12 11:33:59 -0500

    Removed unnecessary items

--- Program.cs
+++ Program.cs
@@ -3,20 +3,20 @@
     static async System.Threading.Tasks.Task Main(string[] args) 
     {
         string randomUrl = "https://en.wikipedia.org/wiki/Special:Random";
-        Webpage[] webpages = new Webpage[11];
-        for (int i = 0; i < 9; i++) 
+        Webpage[] webpages = new Webpage[101];
+        for (int i = 0; i < 100; i++) 
         {
             webpages[i] = await Webpage.BuildWebpageAsync(randomUrl);
         }
-        webpages[9] = await Webpage.BuildWebpageAsync("https://en.wikipedia.org/wiki/Software_engineering");
+        webpages[99] = await Webpage.BuildWebpageAsync("https://en.wikipedia.org/wiki/Software_engineering");
         string? userUrl = System.Console.ReadLine();
         if (userUrl is null)
             return;
-        webpages[10] = await Webpage.BuildWebpageAsync(userUrl);
+        webpages[100] = await Webpage.BuildWebpageAsync(userUrl);
         var matrix = TfidfVectorizer.Vectorize(webpages);
-        var tfidfVector = matrix[10];
-        var trueMatrix = new double[10][]; 
-        for (int i = 0; i < 10; ++i)
+        var tfidfVector = matrix[100];
+        var trueMatrix = new double[100][]; 
+        for (int i = 0; i < 100; ++i)
         {
             trueMatrix[i] = matrix[i];
         }
